381d3b2110a7a1cc63237ddcfea5620d43f1cd8e
Raphael Ichiro <spookdrumbstep4200@gmail.com>
2025-07-20 22:42:27 -0300

    feat: inserção de caminhos para as novas páginas

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,6 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router';
 import CadastroUsuario from '../views/CadastroUsuarioView.vue';
 import HomeView        from '../views/HomeView.vue';
 import LocalDetalheView from '../views/LocalDetalheView.vue';
+import AtividadesView from '../views/AtividadesView.vue';
+import InstalacoesView from '../views/InstalacoesView.vue';
+import LogsView from '../views/LogsView.vue';
+import UsuariosView from '../views/UsuariosView.vue';
+import PerfilView from '../views/PerfilView.vue';
+import LoginView from '../views/LoginView.vue';
+import RecuperarSenhaView from '../views/RecuperarSenhaView.vue';
+import NovaSenhaView from '../views/NovaSenhaView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -29,6 +37,46 @@ const router = createRouter({
       name: 'local-detalhe',
       component: LocalDetalheView,
       props: true
+    },
+    {
+      path: '/atividades',
+      name: 'atividades',
+      component: AtividadesView,
+    },
+    {
+      path: '/instalacoes',
+      name: 'instalacoes',
+      component: InstalacoesView,
+    },
+    {
+      path: '/logs',
+      name: 'logs',
+      component: LogsView,
+    },
+    {
+      path: '/usuarios',
+      name: 'usuarios',
+      component: UsuariosView,
+    },
+    {
+      path: '/perfil',
+      name: 'perfil',
+      component: PerfilView,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+    },
+    {
+      path: '/recuperar-senha',
+      name: 'recuperar-senha',
+      component: RecuperarSenhaView,
+    },
+    {
+      path: '/nova-senha',
+      name: 'nova-senha',
+      component: NovaSenhaView,
     },
   ],
 })
