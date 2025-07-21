f16ef43586d94174265eb6879e635a9e74fe5a4f
Raphael Ichiro <spookdrumbstep4200@gmail.com>
2025-07-20 22:18:11 -0300

    feat: página de instalações e tabela de instalações

---
+++ frontend/src/views/InstalacoesView.vue
@@ -0,0 +1,31 @@
+<template>
+  <PesquisarLocal @abrir-dialog="abrirDialog" />
+  <DialogCadastro
+    v-if="modalCadastroVisivel && tipoCadastro === 'instalacao'"
+    :visible="modalCadastroVisivel"
+    tipo="instalacao"
+    @update:visible="modalCadastroVisivel = $event"
+  />
+  <DialogCadastro
+    v-if="modalCadastroVisivel && tipoCadastro === 'atividade'"
+    :visible="modalCadastroVisivel"
+    tipo="atividade"
+    @update:visible="modalCadastroVisivel = $event"
+  />
+  <div style="margin-top: 32px;"><InstalacoesTable /></div>
+</template>
+
+<script setup>
+import PesquisarLocal from '@/components/PesquisarLocal.vue';
+import DialogCadastro from '@/components/DialogCadastro/DialogCadastro.vue';
+import InstalacoesTable from '@/components/InstalacoesTable.vue';
+import { ref } from 'vue';
+
+const modalCadastroVisivel = ref(false);
+const tipoCadastro = ref('instalacao');
+
+function abrirDialog(tipo) {
+  tipoCadastro.value = tipo;
+  modalCadastroVisivel.value = true;
+}
+</script> 
