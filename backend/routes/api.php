a46139825426f48973d30f2a7dd3f0aa1d89c9a0
Raphael Ichiro <spookdrumbstep4200@gmail.com>
2025-07-28 00:10:12 -0300

    feat: adição de mais rotas

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,11 @@ use App\Http\Controllers\AtividadeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FuncaoController;
 use App\Http\Controllers\InstalacaoController;
+use App\Http\Controllers\LocalController;
+use App\Http\Controllers\LocalImagemController;
+use App\Http\Controllers\LocalAtividadeController;
+use App\Http\Controllers\LocalInstalacaoController;
+use App\Http\Controllers\LocalAvaliacaoController;
 use App\Http\Controllers\SecaoController;
 use App\Http\Controllers\UsuarioController;
 
@@ -21,3 +26,31 @@ Route::post("/atividade", [AtividadeController::class, "store"]);
 
 Route::get("/instalacao", [InstalacaoController::class, "index"]);
 Route::post("/instalacao", [InstalacaoController::class, "store"]);
+
+Route::get("/local", [LocalController::class, "index"]);
+Route::post("/local", [LocalController::class, "store"]);
+Route::get("/local/{local}", [LocalController::class, "show"]);
+Route::put("/local/{local}", [LocalController::class, "update"]);
+Route::delete("/local/{local}", [LocalController::class, "destroy"]);
+
+// Rotas para imagens de locais
+Route::get("/local/{local}/imagens", [LocalImagemController::class, "index"]);
+Route::get("/local-imagem/{localImagem}", [LocalImagemController::class, "show"]);
+Route::post("/local-imagem", [LocalImagemController::class, "store"]);
+Route::delete("/local-imagem/{localImagem}", [LocalImagemController::class, "destroy"]);
+
+// Rotas para atividades de locais
+Route::get("/local/{local}/atividades", [LocalAtividadeController::class, "index"]);
+Route::post("/local-atividade", [LocalAtividadeController::class, "store"]);
+Route::delete("/local-atividade/{localAtividade}", [LocalAtividadeController::class, "destroy"]);
+
+// Rotas para instalações de locais
+Route::get("/local/{local}/instalacoes", [LocalInstalacaoController::class, "index"]);
+Route::post("/local-instalacao", [LocalInstalacaoController::class, "store"]);
+Route::delete("/local-instalacao/{localInstalacao}", [LocalInstalacaoController::class, "destroy"]);
+
+// Rotas para avaliações de locais
+Route::get("/local/{local}/avaliacoes", [LocalAvaliacaoController::class, "index"]);
+Route::post("/local-avaliacao", [LocalAvaliacaoController::class, "store"]);
+Route::put("/local-avaliacao/{localAvaliacao}", [LocalAvaliacaoController::class, "update"]);
+Route::delete("/local-avaliacao/{localAvaliacao}", [LocalAvaliacaoController::class, "destroy"]);
